52b8b79282336005515a75c97e737273fb61772c
Aaron <tim2464@yahoo.com.tw>
2020-04-18 12:06:19 -0700

    [Assignment2]

---
+++ Assignment2/Assignment2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by AaronH on 2020-04-17.
+//  Copyright © 2020 AaronH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
