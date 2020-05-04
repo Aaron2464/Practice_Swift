b711a3252f44d60df20e0d9fd086a100516bc219
Aaron <tim2464@yahoo.com.tw>
2020-05-04 16:17:20 -0700

    EmojiDictionary, ScrollingForm, SimpleTodoApp and TableViewDemoCode

---
+++ SimpleTodoApp/SimpleTodoApp/EditTodoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EditTodoViewController.swift
+//  SimpleTodoApp
+//
+//  Created by AaronH on 2020-05-04.
+//  Copyright © 2020 AaronH. All rights reserved.
+//
+
+import UIKit
+
+class EditTodoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ SimpleTodoApp/SimpleTodoApp/TodoListViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TodoListViewController.swift
+//  SimpleTodoApp
+//
+//  Created by AaronH on 2020-05-01.
+//  Copyright © 2020 AaronH. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+// Change header
+//    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        return "Header \(section)"
+//    }
+    
+}
