85b78406f60345ab996807e85f7b5cd9a9d4f53f
Aaron <tim2464@yahoo.com.tw>
2020-05-08 16:38:41 -0700

    [Assignment 6] Show list

--- SimpleTodoApp/SimpleTodoApp/EditTodoViewController.swift
+++ SimpleTodoApp/SimpleTodoApp/EditTodoViewController.swift
@@ -29,8 +29,8 @@ class EditTodoViewController: UIViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.destination is TodoListViewController{
-            let vc = segue.destination as? TodoListViewController
-            vc?.list?.append(descriptionTextField.text ?? " ")
+//            let vc = segue.destination as! TodoListViewController
+//            TodoListViewController.list[2].append(descriptionTextField.text ?? "")
         }
     }
 }
--- SimpleTodoApp/SimpleTodoApp/TodoListViewController.swift
+++ SimpleTodoApp/SimpleTodoApp/TodoListViewController.swift
@@ -10,11 +10,56 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    var list: [String]?
+    
+    var list: [[String]] = [["Build up App"], ["Eat dinner"], ["Sleep all night"]]
+   
     override func viewDidLoad() {
         super.viewDidLoad()
+        let fullScreenSize = UIScreen.main.bounds.size
         
-        print(list as Any)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "HighCell")
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "MidCell")
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "LowCell")
+
+        tableView.delegate = self
+        // 分隔線的樣式
+        tableView.separatorStyle = .singleLine
+
+        // 分隔線的間距 四個數值分別代表 上、左、下、右 的間距
+        tableView.separatorInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+
+        // 是否可以點選 cell
+        tableView.allowsSelection = true
+
+        // 是否可以多選 cell
+        tableView.allowsMultipleSelection = false
+
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list[section].count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MidCell", for: indexPath) as UITableViewCell
+        
+        if indexPath.section == 0{
+            if indexPath.row == 0 {
+                cell.accessoryType = .checkmark
+            } else if indexPath.row == 1 {
+                cell.accessoryType = .detailButton
+            } else if indexPath.row == 2 {
+                cell.accessoryType = .detailDisclosureButton
+            } else if indexPath.row == 3 {
+                cell.accessoryType = .disclosureIndicator
+            }
+        }
+        
+        if let myLabel = cell.textLabel {
+            myLabel.text = "\(list[indexPath.section][indexPath.row])"
+        }
+        
+        return cell
     }
     
 // Change header
@@ -24,7 +69,7 @@ class TodoListViewController: UITableViewController {
     
     @IBAction func unwindToListView(segue: UIStoryboardSegue){
         if segue.identifier == EditTodoViewController.unwindSegueId {
-
+//            print(TodoListViewController.list as Any)
         }
     }
 }
